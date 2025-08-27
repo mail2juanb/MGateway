68f4e6bab4efe66f1f2e93db3b943089b8397107
juanb <mail2juanb@yahoo.fr>
2025-08-27 23:30:59 +0200

    Initial commit

---
+++ src/test/java/com/microdiab/MGateway/MGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microdiab.MGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
