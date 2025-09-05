51ad40778c923e066d9515dda4b8e21dc37f3b74
juanb <mail2juanb@yahoo.fr>
2025-09-05 17:51:03 +0200

    Moove to better place for SecurityConfig

--- src/main/java/com/microdiab/mgateway/configuration/SecurityConfig.java
+++ src/main/java/com/microdiab/mgateway/configuration/SecurityConfig.java
@@ -2,14 +2,14 @@ package com.microdiab.mgateway.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
