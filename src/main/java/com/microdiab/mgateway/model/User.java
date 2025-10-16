95c4c8ca41e60b27985663849a2048ed066eaf12
juanb <mail2juanb@yahoo.fr>
2025-10-16 23:56:03 +0200

    Add users MySQL Database : Dependances / application.properties / schema.sql / data.sql

---
+++ src/main/java/com/microdiab/mgateway/model/User.java
@@ -0,0 +1,82 @@
+package com.microdiab.mgateway.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private Long id;
+
+    @NotBlank(message = "username is mandatory")
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message = "password is mandatory")
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+
+    // Getter and Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // Constructor
+    public User() {
+    }
+
+    public User(Long id, String username, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // ToString
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, username, password, role) VALUES
+    ('1', 'user', '$2a$10$X5eEon.VJnN1S8nggslRgeHLR.oNfQRvMm4TY/ENhbJbBmJ/Jf6Y.', 'USER'),
+    ('2', 'admin', '$2a$10$X5eEon.VJnN1S8nggslRgeHLR.oNfQRvMm4TY/ENhbJbBmJ/Jf6Y.', 'ADMIN');
+
+    --     password user
